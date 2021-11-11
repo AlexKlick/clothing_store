a083ba2260d9c741839e701a2a069b7e618281eb
Alex Klick <aklick12@gmail.com>
2021-11-11 12:51:31 -0700

    collection preview and collection item with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Routes } from "react-router";
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shop" element={<ShopPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route } from "react-router-dom";
-import { Routes } from "react-router";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
