5b3038210776152710dcc90eeff33c46af558038
Alex Klick <aklick12@gmail.com>
2021-12-03 19:12:58 -0700

    userReducer and root reducer setup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
